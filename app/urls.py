c6faff9eb3add3426ab14b0744df0f1828a4439e
Bohdan Marko <goldenbohdanm@gmail.com>
2023-10-06 20:13:33 +0300

    Implemented a CRUD Views for Tag model + configure static for the FrontEnd

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
-from app.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDoneUpdateView, TaskDeleteView
+from app.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDoneUpdateView, TaskDeleteView, TagListView, \
+    TagCreateView, TagUpdateView, TagDeleteView
 
 app_name = "app"
 
@@ -10,5 +11,8 @@ urlpatterns = [
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/update-done/", TaskDoneUpdateView.as_view(), name="task-confirm-done"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from app.forms import TaskForm
-from app.models import Task
+from app.models import Task, Tag
 
 
 # Create your views here.
@@ -36,5 +36,32 @@ class TaskDoneUpdateView(generic.UpdateView):
 class TaskDeleteView(generic.DeleteView):
     model = Task
     fields = "__all__"
-    template_name = "task_confirm_delete.html"
+    template_name = "app/task_confirm_delete.html"
     success_url = reverse_lazy("todo-list:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+    template_name = "app/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "app/tag_form.html"
+    success_url = reverse_lazy("todo-list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "app/tag_form.html"
+    success_url = reverse_lazy("todo-list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    fields = "__all__"
+    template_name = "app/tag_confirm_delete.html"
+    success_url = reverse_lazy("todo-list:tag-list")
