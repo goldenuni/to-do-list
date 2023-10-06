33a7a8872d54d7fb232e8d1250db4e41b41b68fa
Bohdan Marko <goldenbohdanm@gmail.com>
2023-10-06 19:40:34 +0300

    Implement Update % Create View for Tag model

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 
-from app.views import TaskListView
+from app.views import TaskListView, TaskCreateView, TaskUpdateView
 
 app_name = "app"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from app.forms import TaskForm
 from app.models import Task
 
 
@@ -9,3 +11,17 @@ class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
     template_name = "app/task_list.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "app/task_form.html"
+    success_url = reverse_lazy("todo-list:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "app/task_form.html"
+    success_url = reverse_lazy("todo-list:task-list")
