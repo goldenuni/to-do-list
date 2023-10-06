d87c4443a2d9572767bba7c81c3b612bc4323b35
Bohdan Marko <goldenbohdanm@gmail.com>
2023-10-06 18:57:02 +0300

    Create a models & add the app in settings.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(unique=True, max_length=256)
+
+    class Meta:
+       ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["done", "datetime"]
